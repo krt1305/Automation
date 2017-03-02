6a65914b998e1011abfaca5b2044e94a7b4f86d3
krt1305 <krt1305@gmail.com>
2017-03-02 22:16:56 +0530

    changed user name

--- src/com/practo/testcases/Test1.java
+++ src/com/practo/testcases/Test1.java
@@ -1,12 +1,18 @@
 package com.practo.testcases;
 import org.testng.annotations.Test;
 import com.practo.base.BaseClass;
+import com.practo.util.ExtentManager;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
 
 public class Test1 extends BaseClass{
 
 	@Test
 	public void function1()
 	{
+		
+		ExtentReports rep=ExtentManager.getInstance();
+		ExtentTest test=rep.startTest("Started Test");	
 		System.out.println("Hello");
 	}
 	
